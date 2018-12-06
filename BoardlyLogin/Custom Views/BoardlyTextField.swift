92d9769d309fd076de33e0f371c49d0db2068f1e
Mateusz Dziubek <matdziu@gmail.com>
2018-12-06 14:42:32 +0100

    Added custom text field

---
+++ BoardlyLogin/Custom Views/BoardlyTextField.swift
@@ -0,0 +1,20 @@
+//
+//  BoardlyTextField.swift
+//  BoardlyLogin
+//
+//  Created by Mateusz Dziubek on 06/12/2018.
+//  Copyright © 2018 Mateusz Dziubek. All rights reserved.
+//
+
+import UIKit
+
+class BoardlyTextField: UITextField {
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.borderColor = UIColor(named: Color.primaryBlue.rawValue)?.cgColor
+        layer.borderWidth = 1.0
+        clipsToBounds = true
+        layer.cornerRadius = frame.size.height / 5.0
+    }
+}
