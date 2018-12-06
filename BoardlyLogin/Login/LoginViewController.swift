c1e6ee2beb94d055b4b5edfb373e80a7098b1de1
Mateusz Dziubek <matdziu@gmail.com>
2018-12-06 13:30:22 +0100

    Initial commit

---
+++ BoardlyLogin/Login/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  BoardlyLogin
+//
+//  Created by Mateusz Dziubek on 06/12/2018.
+//  Copyright © 2018 Mateusz Dziubek. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
