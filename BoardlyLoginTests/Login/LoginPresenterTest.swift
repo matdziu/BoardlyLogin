ceac5485d84a895efd5fff3e1a4303c32f75df89
Mateusz Dziubek <matdziu@gmail.com>
2018-12-07 13:55:10 +0100

    Added empty test classes

---
+++ BoardlyLoginTests/Login/LoginPresenterTest.swift
@@ -0,0 +1,13 @@
+//
+//  LoginPresenterTest.swift
+//  BoardlyLoginTests
+//
+//  Created by Mateusz Dziubek on 07/12/2018.
+//  Copyright © 2018 Mateusz Dziubek. All rights reserved.
+//
+
+import Foundation
+
+class LoginPresenterTest {
+    
+}
