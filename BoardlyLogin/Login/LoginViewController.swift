4805c305bf9a0dd31cf69d1fa67b3b44c2d013c7
Mateusz Dziubek <matdziu@gmail.com>
2018-12-07 09:59:30 +0100

    Added rx cocoa emitter

--- BoardlyLogin/Login/LoginViewController.swift
+++ BoardlyLogin/Login/LoginViewController.swift
@@ -7,12 +7,29 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-class LoginViewController: BaseNavViewController {
-
+class LoginViewController: BaseNavViewController, LoginView {
+    
+    @IBOutlet weak var loginButton: BoardlyButton!
+    @IBOutlet weak var emailField: BoardlyTextField!
+    @IBOutlet weak var passwordField: BoardlyTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        LoginPresenter(loginInteractor: LoginInteractorImpl(loginService: LoginServiceImpl())).bind(loginView: self)
+    }
+    
+    func inputEmitter() -> Observable<InputData> {
+        return loginButton
+            .rx
+            .controlEvent(UIControl.Event.touchUpInside)
+            .map({ InputData(email: self.emailField.text ?? "", password: self.passwordField.text ?? "") })
+    }
+    
+    func render(loginViewState: LoginViewState) {
+        
     }
 }
 
